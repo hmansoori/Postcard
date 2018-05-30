266ec801a42962f1d4885b14534a977c27ed14af
Patricia Au <patau@uw.edu>
2018-05-29 22:24:25 -0700

    Changed routes

--- postcards/src/constants/routes.js
+++ postcards/src/constants/routes.js
@@ -1,6 +1,6 @@
-export const SIGN_UP = process.env.PUBLIC_URL + '/signup';
-export const SIGN_IN = process.env.PUBLIC_URL + '/signin';
-export const LANDING = process.env.PUBLIC_URL + '/';
-export const GROUP = process.env.PUBLIC_URL + '/groups';
-export const ACCOUNT = process.env.PUBLIC_URL + '/account';
-export const PASSWORD_FORGET = process.env.PUBLIC_URL + '/pw-forget';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const LANDING = '/';
+export const GROUP = '/groups';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/pw-forget';

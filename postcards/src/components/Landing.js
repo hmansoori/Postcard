02d04527d2aa3d0a14cdb0f286f07102d2c4c5ff
Hannah <hmansoori97@gmail.com>
2018-05-30 13:01:31 -0700

    blah

--- postcards/src/components/Landing.js
+++ postcards/src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button, FormGroup, FormControl } from 'react-bootstrap';
+import { Button, FormGroup, FormControl, } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 import logo from './images/pc_logo.png';
 import * as routes from '../constants/routes';
@@ -11,7 +12,7 @@ const LandingPage = () =>
     <h4>A zero-clutter social media site that closes the generation gap.</h4>
     <br></br>
     <p>Start sharing now by signing in or clicking the groups tab above.</p>
-    <Button href={routes.SIGN_IN} bsStyle="primary">Get Started!</Button>
+    <Button href={routes.SIGN_IN} bsStyle="primary"><Link to={routes.SIGN_IN} className="welcome-link">Get Started!</Link></Button>
   </div>
 
 export default LandingPage;
